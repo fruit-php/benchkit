cbb29bf21e5d27c932bb0b860cf34ef43d4cab60
Ronmi Ren <ronmi.ren@gmail.com>
2015-10-22 17:52:43 +0800

    update docs

--- search/all_6.js
+++ search/all_6.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['benchkit',['BenchKit',['../namespace_fruit_1_1_bench_kit.html',1,'Fruit']]],
   ['bin',['Bin',['../namespace_fruit_1_1_bench_kit_1_1_bin.html',1,'Fruit::BenchKit']]],
-  ['format',['format',['../class_fruit_1_1_bench_kit_1_1_formatter_1_1_default_progress_logger.html#adee94865b7767ef2b2e935a5e521fdc6',1,'Fruit\BenchKit\Formatter\DefaultProgressLogger\format()'],['../class_fruit_1_1_bench_kit_1_1_formatter_1_1_default_summary_logger.html#a9ba12ae0472baa9a90abccc262b81b97',1,'Fruit\BenchKit\Formatter\DefaultSummaryLogger\format()'],['../interface_fruit_1_1_bench_kit_1_1_formatter_1_1_progress.html#adee94865b7767ef2b2e935a5e521fdc6',1,'Fruit\BenchKit\Formatter\Progress\format()'],['../interface_fruit_1_1_bench_kit_1_1_formatter_1_1_summary.html#ac702899db9c6f482a8c670b02405aee3',1,'Fruit\BenchKit\Formatter\Summary\format()']]],
+  ['format',['format',['../class_fruit_1_1_bench_kit_1_1_formatter_1_1_default_progress_logger.html#a5f54c519070118f37307eed7e61f5ba7',1,'Fruit\BenchKit\Formatter\DefaultProgressLogger\format()'],['../class_fruit_1_1_bench_kit_1_1_formatter_1_1_default_summary_logger.html#a9ba12ae0472baa9a90abccc262b81b97',1,'Fruit\BenchKit\Formatter\DefaultSummaryLogger\format()'],['../interface_fruit_1_1_bench_kit_1_1_formatter_1_1_progress.html#a5f54c519070118f37307eed7e61f5ba7',1,'Fruit\BenchKit\Formatter\Progress\format()'],['../interface_fruit_1_1_bench_kit_1_1_formatter_1_1_summary.html#ac702899db9c6f482a8c670b02405aee3',1,'Fruit\BenchKit\Formatter\Summary\format()']]],
   ['formatter',['Formatter',['../namespace_fruit_1_1_bench_kit_1_1_formatter.html',1,'Fruit::BenchKit']]],
   ['fruit',['Fruit',['../namespace_fruit.html',1,'']]]
 ];
